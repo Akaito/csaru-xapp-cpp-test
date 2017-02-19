b0d11b287822e57968b3a9139d12ae9948722588
Chris Barrett <chris@codesaru.com>
2017-02-19 13:27:13 -0800

    Test clearing/rendering windows.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+#define SDL_MAIN_HANDLED
 #ifdef WIN32
 #	include <SDL.h>
-#	undef main
 #else
 #	include <SDL2/SDL.h>
 #endif
@@ -8,15 +8,30 @@
 #include <csaru-xapp-cpp/everything.hpp>
 
 int main (int argc, char * argv[]) {
+	SDL_SetMainReady();
 	argc; argv;
+
+	csaru::xapp::Application app;
+	if (!app.Init())
+		return 1;
 
 	csaru::xapp::Window winA;
 	csaru::xapp::Window winB;
 
 	winA.Init("Win A", 1366, 768);
-	winB.Init("Win B",  640, 480);
+	app.AddWindow(&winA);
 
-	SDL_Delay(2000);
+	winB.Init("Win B",  640, 480);
+	app.AddWindow(&winB);
+
+	while (app.HasOpenWindows()) {
+		winA.Clear();
+		winB.Clear();
+		winA.Render();
+		winB.Render();
+
+		SDL_Delay(16); // 16 milliseconds
+	}
 
 	return 0;
 }
